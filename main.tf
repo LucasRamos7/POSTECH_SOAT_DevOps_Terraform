1f108bf2ee812a6fcd9c1aec60c36fc3565d9218
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-03 23:34:57 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region        = "us-east-1"
+  access_key    = var.AWS_ACCESS_KEY_ID
+  secret_key    = var.AWS_SECRET_ACCESS_KEY
   session_token = var.AWS_SESSION_TOKEN
 }
 
